59dff32d4bf115614853ba3a8a420e295fe0ac6c
htadashi <hugo.kussaba@tum.de>
2023-09-01 18:33:51 +0200

    Minor adjustments to Panda Animation.m

--- Panda_Animation.m
+++ Panda_Animation.m
@@ -6,22 +6,25 @@ robot.DataFormat = 'column';
 
 % Home Configuration
 Config_home = homeConfiguration(robot);
-figure
-show(robot,Config_home,'Frames','off');
-title("Home Configurationn")
+%figure
+%show(robot,Config_home,'Frames','off');
+%title("Home Configurationn")
 
 % Animation
-s = 50;
+s = 100;
 theta_1 = linspace(0,pi/3,s);
-theta_2 = linspace(0,pi/3,s);
-theta_4 = linspace(0,-pi/3,s);
-theta_6 = linspace(0,2*pi/3,s);
-theta_7 = linspace(0,2*pi/3,s);
+theta_2 = linspace(0,0,s);
+theta_3 = linspace(0,0,s);
+theta_4 = linspace(0,0,s);
+theta_5 = linspace(0,0,s);
+theta_6 = linspace(0,0,s);
+theta_7 = linspace(0,0,s);
 
 figure
 for i=1:s
-    configNow = [theta_1(i) theta_2(i) 0 theta_4(i) 0 theta_6(i) theta_7(i) 0 0].';
-    show(robot,configNow,'PreservePlot',false,'Frames','off');
+    configNow = [0 0 0 0 0 0 theta_7(i) 0 0].';
+    HTM_EE = getTransform(robot, configNow, 'panda_hand');
+    show(robot,configNow,'FastUpdate', true, 'PreservePlot',false,'Frames','off');
     
     % Supporting Plane
     hold on
